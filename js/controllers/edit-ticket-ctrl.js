117d14318b01834e7694c1bd7f9e0d1310cccccd
unknown <denis.azaare@wsb-082.icx.local>
2014-09-17 14:50:16 +0300

    no message

--- js/controllers/edit-ticket-ctrl.js
+++ js/controllers/edit-ticket-ctrl.js
@@ -1,15 +1,11 @@
 app.controller('EditTicketCtrl', function ($scope, $stateParams, $http, Ticket) {
         var myId = parseInt($stateParams.id);
-        //console.log($stateParams.id)
-        Ticket.get({id:"5419320ee4b075452ab4ef40"}).$promise.
-        //Ticket.query({q:{id:myId}}).$promise.
+        //Ticket.get({id:"5419320ee4b075452ab4ef40"}).$promise.
+        Ticket.query({q:{id:myId}}).$promise.
             then(function(result){
-                console.log(result);
                 $scope.edit_bug = result;
 
                  $scope.edit = function(edit_bug){
-                    //alert(edit_bug._id )
-                    //edit_bug.id = $stateParams.id
                     delete edit_bug._id ;
                     Ticket.update({_id:edit_bug._id.$oid},edit_bug)
                 };
@@ -19,12 +15,8 @@ app.controller('EditTicketCtrl', function ($scope, $stateParams, $http, Ticket) 
                 };
 
             });
-       
-
-        this.comments = {};
-
-
-        $scope.addReview = function (ticket) {
+            this.comments = {};
+            $scope.addReview = function (ticket) {
             var commentData = {
                 comment_author: $scope.ticket.comment_author || '',
                 comment: $scope.ticket.comment || '',
